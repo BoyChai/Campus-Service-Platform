316f2edf41a03cafd56a5a35f92e0b0c1affd85a
BoyChai <1972567225@qq.com>
2023-11-02 19:35:43 +0800

    Optimize data table structure

--- dao/struct.go
+++ dao/struct.go
@@ -17,29 +17,37 @@ type User struct {
 	// ID用户的唯一标识
 	ID uint `gorm:"primaryKey;auto_increment"`
 	// 用户名
-	Name string
+	Name string `gorm:"not null"`
 	// 电话号
-	Number string `gorm:"unique"`
+	Number string `gorm:"unique;not null"`
 	// 密码(md5(Pass))
-	Pass string
+	Pass string `gorm:"not null"`
 	// 图片地址
-	ImgUrl string
+	ImgUrl string `gorm:"not null"`
 	// 微信id
 	WxID string `gorm:"unique"`
 	// 角色
-	Role Role
+	Role Role `gorm:"not null"`
 }
 
 // WorkOrder 工单表
 type WorkOrder struct {
-	ID         uint `gorm:"primaryKey;auto_increment"`
-	CreateUser uint
-	OrderType  OrderType
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-	DeletedAt  gorm.DeletedAt `gorm:"index"`
+	// ID 订单的唯一标识
+	ID uint `gorm:"primaryKey;auto_increment"`
+	// CreateUser 创建订单的用户id
+	CreateUser uint `gorm:"not null"`
+	// OrderType 订单类型
+	OrderType OrderType `gorm:"not null"`
+	// CreatedAt 创建时间
+	CreatedAt time.Time
+	// UpdatedAt 订单更新时间
+	UpdatedAt time.Time
+	// DeletedAt 订单删除时间
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	// CompleteAT 订单完成时间
 	CompleteAT time.Time
-	Info       JSON `gorm:"type:json"`
+	// 订单创建时的一些初始信息
+	Info JSON `gorm:"type:json;not null"`
 }
 
 // Role 角色类别
@@ -53,6 +61,8 @@ const (
 	Admin Role = iota
 	// Leader 领导
 	Leader
+	// Work 工作者
+	Work
 	// Ordinary 普通用户
 	Ordinary
 )
