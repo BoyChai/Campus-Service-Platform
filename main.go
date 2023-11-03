16ff5e135778e356e12bd0382a7e961b559aab08
BoyChai <1972567225@qq.com>
2023-11-03 16:19:54 +0800

    Add CORS And JWT Middleware

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"Campus-Service-Platform/dao"
+	"Campus-Service-Platform/middle"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello,World")
+	// 连接数据库
+	dao.Client()
+	// 自动建表
+	dao.AutoTables()
+	// 创建gin
+	r := gin.Default()
+	// 加载中间件
+	r.Use(middle.CORS())
+	r.Use(middle.JWTAuth())
+	// 运行WebService
+	r.Run()
 }
---
+++ middle/auth.go
@@ -0,0 +1,86 @@
+package middle
+
+import (
+	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// SECRET 加解密因子
+const (
+	SECRET = "ikun"
+)
+
+// JWTAuth 中间件，检查token
+func JWTAuth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		//对登录接口放行
+		if len(ctx.Request.URL.String()) >= 6 && ctx.Request.URL.String()[0:6] == "/login" {
+			ctx.Next()
+		} else {
+			//获取Header中的Authorization
+			token := ctx.Request.Header.Get("Authorization")
+			if token == "" {
+				ctx.JSON(http.StatusBadRequest, gin.H{
+					"msg":  "请求未携带token，无权限访问",
+					"data": nil,
+				})
+				ctx.Abort()
+				return
+			}
+			// parseToken 解析token包含的信息
+			claims, err := parseToken(token)
+			if err != nil {
+				//token延期错误
+				if err.Error() == "TokenExpired" {
+					ctx.JSON(http.StatusBadRequest, gin.H{
+						"msg":  "授权已过期",
+						"data": nil,
+					})
+					ctx.Abort()
+					return
+				}
+				//其他解析错误
+				ctx.JSON(http.StatusBadRequest, gin.H{
+					"msg":  err.Error(),
+					"data": nil,
+				})
+				ctx.Abort()
+				return
+			}
+			// 继续交由下一个路由处理,并将解析出的信息传递下去
+			ctx.Set("claims", claims)
+			ctx.Next()
+		}
+	}
+}
+
+// parseToken 解析token
+func parseToken(tokenString string) (user map[string]interface{}, err error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// 校验签名是否被篡改
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("not authorization")
+		}
+		//返回密钥与上面签发时保持一致
+		return []byte(SECRET), nil
+	})
+	if err != nil {
+		fmt.Println("parse token failed ", err)
+		//处理token解析后的各种错误
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return nil, errors.New("TokenMalformed")
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				return nil, errors.New("TokenExpired")
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				return nil, errors.New("TokenNotValidYet")
+			} else {
+				return nil, errors.New("TokenInvalid")
+			}
+		}
+	}
+	return token.Claims.(jwt.MapClaims), nil
+}
